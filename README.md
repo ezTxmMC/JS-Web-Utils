b62fac553da5947c98ec163516e6b16d13e5991e
ezTxmMC <eztxmmc@hotmail.com>
2024-11-14 10:42:30 +0100

    added more methods and added todo to readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # JS-Web-Utils
+
+## ToDo-List
+
+- [ ] Elements
+  - [ ] Normal Text
+  - [ ] Link Text
+  - [ ] Button
+  - [ ] Image
+  - [ ] Form
+  - [ ] Container
+  - [ ] Textarea
+  - [ ] Video
+    - [ ] Source
+  - [ ] Audio
--- popup.js
+++ popup.js
@@ -1,9 +1,47 @@
 class Popup {
+  constructor(id, title) {
+    this.id = id;
+    this.title = title;
+  }
 
-    constructor(id, title) {
-        this.id = id;
-        this.title = title;
-    }
+  addText(value, id, ...classes) {
+    let element = document.createElement("p");
+    element.innerHTML = value;
+    element.id = id;
+    element.classList.add(classes);
+  }
+
+  addLinkText(value, link, id, ...classes) {
+    let element = document.createElement("a");
+    element.innerHTML = value;
+    element.href = link;
+    element.id = id;
+    element.classList.add(classes);
+  }
+
+  addButton(value, onclick, id, ...classes) {
+    let element = document.createElement("button");
+    element.innerHTML = value;
+    element.onclick = onclick;
+    element.id = id;
+    element.classList.add(classes);
+  }
+
+  addImage(source, alt, id, ...classes) {
+    let element = document.createElement("img");
+    element.src = source;
+    element.alt = alt;
+    element.id = id;
+    element.classList.add(classes);
+  }
+
+  addForm(action, method, id, ...classes) {
+    let element = document.createElement("form");
+    element.action = action;
+    element.method = method;
+    element.id = id;
+    element.classList.add(classes);
+  }
 }
 
-module.exports = Popup;
+export default Popup;
